5de4301eb811923cf621995da888cfa1c6c3714e
Tania Ludueña <Tanialuduenaok@gmail.com>
2024-05-08 16:40:07 -0300

    Grabo prueba 2 paso de callback a promesa

---
+++ solutions/index.js
@@ -0,0 +1,73 @@
+// PRUEBA 1 - Arregla esta función para que el código posterior funcione como se espera:
+//Cuando una funcion dice que no hace lo que se espera siempre fijarme en los parametros
+//Intento Ejecutarlo con node solutions/index.js
+
+
+import net from 'node:net'
+
+export const ping = (ip, callback) => { //Aqui agrego la funcion Callback
+    const startTime = process.hrtime()
+
+    const client = net.connect({ port: 80, host: ip }, () => {
+        client.end()
+        //return { time: process.hrtime(startTime), ip } <-- el return que estaba implementado aca no funciona, entonces paso a usar el callback
+        callback(null, { time: process.hrtime(startTime), ip }) //el primer parametro el callback es el error y segundo parametro la info que queremos devolver
+    })
+
+    client.on('error', (err) => { //no olvidar que aqui tambien se aplica el callback
+        callback(err)
+        //throw err <-- Esto tampoco funciona
+        client.end()
+    })
+}
+
+ping('taluok.dev', (err, info) => { //en este caso no se esta ejecutando el callback
+    if (err) console.error(err) //aqui si tiene un error muestra el error y si no muestra la info
+    else console.log(info) //agrego un else
+})
+
+
+// PRUEBA 2 - Transforma la siguiente función para que funcione con promesas en lugar de callbacks:
+
+export function obtenerDatosPromise() {//Quito el Callback y empleo un return
+    return new Promise(function (resolve, reject) {
+        setTimeout(() => {
+            try {
+                resolve({ data: 'datos importantes' });//quito el callback y agrego el resolve
+            } catch (e) {
+                reject(e);
+            }
+        }, 2000);
+    });
+}
+
+obtenerDatosPromise()
+    .then(info => {
+        console.log(info)
+    })
+
+// PRUEBA 3 - Explica qué hace la funcion.Identifica y corrige los errores en el siguiente código.Si ves algo innecesario, elimínalo.Luego mejoralo para que siga funcionando con callback y luego haz lo que consideres para mejorar su legibilidad.
+
+export function procesarArchivo() {
+    fs.readFile('input.txt', 'utf8', (error, contenido) => {
+        if (error) {
+            console.error('Error leyendo archivo:', error.message);
+            return false;
+        }
+
+        setTimeout(() => {
+            const textoProcesado = contenido.toUpperCase();
+
+            fs.writeFile('output.txt', textoProcesado, error => {
+                if (error) {
+                    console.error('Error guardando archivo:', error.message);
+                    return false;
+                }
+
+                console.log('Archivo procesado y guardado con éxito');
+                return true
+            });
+
+        }, 1000);
+    });
+}
---
+++ test/index.test.js
@@ -0,0 +1,24 @@
+import { ping, obtenerDatosPromise, procesarArchivoPromise, procesarArchivo, leerArchivos } from "../solutions/index.js";
+
+import { describe, it, beforeEach, afterEach } from 'node:test'
+import { equal, ifError } from 'node:assert/strict'
+import { unlinkSync, writeFileSync } from 'node:fs'
+import { readFile } from 'node:fs/promises'
+import { createRequire } from 'node:module'
+
+describe('1. ping', () => { //creo un grupo de pruebas relacionada con ping
+    it('1.1. ping taluok.dev', (_, done) => {//it define una prueba individual, la llamada del 1.1 ping verifica el comportaminto de la funcion ping cuanbdo se pasa el argumento taluok.dev
+        ping('taluok.dev', (err, info) => { // La función ping espera un callback que manejará el resultado de la operación.
+            ifError(err) //ifError verifica si hay un error.
+            equal(info.ip, 'taluok.dev') //equal es una función de aserción que se utiliza para comparar dos valores.
+            done() //la llamo al final de la prueba para indicar que la prueba ha finalizado.
+        })
+    })
+})
+
+describe('2. obtenerDatosPromise', () => {
+    it('2.1. obtenerDatosPromise', async () => {
+        const { data } = await obtenerDatosPromise({ time: 1 }) //Se llama a la función obtenerDatosPromise con un objeto { time: 1 } como argumento. La función await espera a que la Promesa devuelta por obtenerDatosPromise se resuelva y luego desestructura el resultado para obtener la propiedad data.
+        equal(data, 'datos importantes') //equal es una función de aserción que se utiliza para comparar dos valores.
+    })
+})
