b8fb37a3d2259bb6c59764decf34b191d2d7b6b1
Tania Ludueña <Tanialuduenaok@gmail.com>
2024-05-24 16:21:10 -0300

    Continuo con el agregado de estados a la ui

--- Practica-FullStack-React/Frontend/vite-project/src/App.tsx
+++ Practica-FullStack-React/Frontend/vite-project/src/App.tsx
@@ -1,45 +1,67 @@
 import React, { useState } from 'react';
-import './App.css'
+import './App.css';
 
 const APP_STATUS = {
-  IDLE: 'idle', //cuando entra 
-  ERROR: 'error', // cuando aparece un error 
+  IDLE: 'idle', // cuando entra
+  ERROR: 'error', // cuando aparece un error
   READY_UPLOAD: 'ready_upload', // cuando se elige un archivo
   UPLOADING: 'uploading', // mientras el archivo se sube
-  READY_USAGE: 'ready_usage', //despues de subir el archivo
-} as const
+  READY_USAGE: 'ready_usage', // después de subir el archivo
+} as const;
 
+const BUTTON_TEXT = {
+  [APP_STATUS.READY_UPLOAD]: 'Subir Archivo',
+  [APP_STATUS.UPLOADING]: 'Subiendo Archivo',
+};
+
+type AppStatusType = typeof APP_STATUS[keyof typeof APP_STATUS];
 
 function App() {
-
-  const [appStatus, setAppStatus] = useState(APP_STATUS.IDLE)
-  ]
+  const [appStatus, setAppStatus] = useState<AppStatusType>(APP_STATUS.IDLE);
+  const [file, setFile] = useState<File | null>(null);
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const files = event.target.files;
     if (files && files.length > 0) {
-      setFile(files[0]);
+      const selectedFile = files[0];
+      setFile(selectedFile);
+      setAppStatus(APP_STATUS.READY_UPLOAD);
     }
   };
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault(); // Evita que el formulario se envíe de forma predeterminada
     
+    if (appStatus !== APP_STATUS.READY_UPLOAD || !file) { //si no se puede hacer nada 
+      return
+    }
+    //si si se puede hacer
+    setAppStatus(APP_STATUS.UPLOADING)
+
   };
+
+  const showButton = appStatus === APP_STATUS.READY_UPLOAD || appStatus === APP_STATUS.UPLOADING;
 
   return (
     <>
       <h4>Upload CSV + Search</h4>
       <form onSubmit={handleSubmit}>
         <label>
-          <input onChange={handleInputChange} name="file" type="file" accept=".csv" />
+          <input
+            disabled={appStatus === APP_STATUS.UPLOADING}
+            onChange={handleInputChange}
+            name="file"
+            type="file"
+            accept=".csv"
+          />
         </label>
-        <button type="submit">Subir Archivo</button>
+        {showButton && (
+          <button disabled={appStatus === APP_STATUS.UPLOADING}>{BUTTON_TEXT[appStatus as keyof typeof BUTTON_TEXT]}</button>
+        )}
       </form>
       {file && (
         <div>
           <p>Archivo seleccionado: {file.name}</p>
-          {/* Aquí puedes agregar más lógica para procesar el archivo */}
         </div>
       )}
     </>
